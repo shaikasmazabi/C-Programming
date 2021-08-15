fdaaa4e01d342da1015385a8718e0f7c378d87ef
SHAIK ASMA ZABI <43025852+shaikasmazabi@users.noreply.github.com>
2021-08-15 18:05:21 +0530

    Update Fibonacci-Series.c

--- Fibonacci-Series.c
+++ Fibonacci-Series.c
@@ -1,5 +1,18 @@
-#includZ e<stdio.h>
+#include<stdio.h>
+int fibo(int n);
+
+int fibo(int n)
+{
+    if(n<=1)
+        return n;
+    return fibo(n-1)+fibo(n-2);
+}
+
 void main()
 {
-    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("%d ",fibo(n));
+
 }
